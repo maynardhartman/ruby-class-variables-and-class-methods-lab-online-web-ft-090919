5ee2a125bd1874efb78dbce54c9bc7bc46260aa3
Maynard Hartman <mjhartman@brighthouse.com>
2019-10-07 16:57:06 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song 
+  attr_accessor :name, :artist, :genre
+  
+    @@count = 0 
+    @@genres = []
+    @@artists = []
+    
+    def initialize( name, artist, genre)
+      @@count += 1 #increment count
+      @name = name
+      @artist = artist
+      @genre = genre
+      @@genres << @genre
+      @@artists << @artist
+      @@genres_count = {}
+      @@artists_count = {}
+    end
+    
+    def self.count
+      @@count
+    end
+    
+    def self.genre_count
+      @@genres.each { |genre|
+      if @@genres_count.key?(genre) == true
+      then
+        @@genres_count[genre] += 1
+      else
+        @@genres_count = {genre => 1}
+      end
+      }
+    @@genres_count
+    end
+    
+    def self.genres
+    end
+    
+    def self.artists
+    end
+  
+    def self.artist_count
+    end
+end
